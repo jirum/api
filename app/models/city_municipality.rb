0de6ae3f450f33aaa87a65b7d99e2344a7689b06
jirum <monteclar.jerome19@gmail.com>
2022-07-14 14:58:17 +0800

    add relationship between barangay and municipality

--- app/models/city_municipality.rb
+++ app/models/city_municipality.rb
@@ -2,7 +2,8 @@ class CityMunicipality < ApplicationRecord
   validates_presence_of :code
   validates_presence_of :name
 
-  belongs_to :region, optional: true
+  belongs_to :province, optional: true
   belongs_to :district, optional: true
+  has_many :barangays
 
 end
